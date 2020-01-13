47b824eb9c52b4ab1ad59e52cbf72ff50007e04b
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 10:12:42 -0700

    Forecast model now initialized with two arguments

--- app/models/Forecast.rb
+++ app/models/Forecast.rb
@@ -1,10 +1,15 @@
 class Forecast
-  def initialize(location)
+  def initialize(location, lat_long)
     @location = location
+    @lat_long = lat_long
   end
 
   def get_weather_forecast
     lat_long = Geocoding.new(@location).latitude_longitude
-    Weather.new(lat_long).current_weather
+    Weather.new(lat_long, 0).current_weather
+  end
+
+  def get_antipode_forecast
+    Weather.new(0, @lat_long[:data][:attributes]).current_weather_antipode
   end
 end
