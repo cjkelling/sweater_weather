3dd8baae18326112640116ba327f49a9affde4d0
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 10:48:55 -0700

    Update controller to submit all information to serializer

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
     antipode_lat_long = Antipode.new(params[:location]).get_antipode
-    weather = Forecast.new(0, antipode_lat_long).get_antipode_forecast
+    antipode_weather_data = Forecast.new(0, antipode_lat_long).get_antipode_forecast
+    render json: AntipodeSerializer.new(AntipodeFacade.new(antipode_weather_data, params[:location]))
   end
 end
