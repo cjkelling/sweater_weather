c5c57afae1f27d1066b1bd0672288754f8948c08
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 09:48:49 -0700

    Remove weather_forcast_antipode method

--- app/models/Forecast.rb
+++ app/models/Forecast.rb
@@ -7,8 +7,4 @@ class Forecast
     lat_long = Geocoding.new(@location).latitude_longitude
     Weather.new(lat_long).current_weather
   end
-
-  def get_weather_forecast_antipode
-    lat_long = Geocoding.new(@location).latitude_longitude
-  end
 end
