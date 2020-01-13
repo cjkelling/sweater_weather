e4166e86cf08b378314aca729956fb93045bd6f0
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 10:13:11 -0700

    Weather model now initializes with two arguments. Add current_weather_antipode method.

--- app/models/Weather.rb
+++ app/models/Weather.rb
@@ -1,10 +1,14 @@
 class Weather
-  def initialize(lat_long)
-    @latitude = lat_long[:lat]
-    @longitude = lat_long[:lng]
+  def initialize(lat_lng, lat_long)
+    @lat_lng = lat_lng
+    @lat_long = lat_long
   end
 
   def current_weather
-    DarkSkyService.new.get_weather(@latitude, @longitude)
+    DarkSkyService.new.get_weather(@lat_lng[:lat], @lat_lng[:lng])
+  end
+
+  def current_weather_antipode
+    DarkSkyService.new.get_weather(@lat_long[:lat], @lat_long[:long])
   end
 end
