62a23f7dfde5af3c047052646a9e625ff3fa3ace
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 11:17:08 -0700

    Update geocoding model to have a reverse lookup action

--- app/models/Geocoding.rb
+++ app/models/Geocoding.rb
@@ -1,9 +1,14 @@
 class Geocoding
-  def initialize(location)
+  def initialize(location, lat_long)
     @location = location
+    @lat_long = lat_long
   end
 
   def latitude_longitude
     GoogleGeocodingService.new.get_latitude_longitude(@location)
+  end
+
+  def reverse_lookup
+    GoogleGeocodingReverseService.new.get_city(@lat_long[:data][:attributes])
   end
 end
