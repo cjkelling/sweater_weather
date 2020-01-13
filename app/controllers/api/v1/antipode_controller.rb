680d7a8479f6f62539c8b0e6d2aff2838de0c5ba
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 10:12:01 -0700

    Update controller to return weather for antipode city

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
     antipode_lat_long = Antipode.new(params[:location]).get_antipode
+    weather = Forecast.new(0, antipode_lat_long).get_antipode_forecast
   end
 end
