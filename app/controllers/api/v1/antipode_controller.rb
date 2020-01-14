a66701b329aa2056a19e76e5433dbf2aad3680f5
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-14 14:07:24 -0700

    Facade update

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AntipodeController < ApplicationController
   def index
     antipode_lat_long = Antipode.new(params[:location]).get_antipode
     antipode_city = Geocoding.new(0, antipode_lat_long).reverse_lookup
-    antipode_weather_data = Forecast.new(0, antipode_lat_long).get_antipode_forecast
-    render json: AntipodeSerializer.new(AntipodeFacade.new(antipode_weather_data, params[:location], antipode_city))
+    render json: AntipodeSerializer.new(AntipodeFacade.new(params[:location], antipode_city))
   end
 end
--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -4,14 +4,22 @@ class AntipodeFacade
               :forecast,
               :search_location
 
-  def initialize(data, location, antipode_city)
+  def initialize(location, antipode_city)
     i = 0
     @id = i + 1
     @location_name = antipode_city
+    @search_location = location
     @forecast = {
       'current_summary' => data[:currently][:summary],
       'current_temperature' => data[:currently][:temperature]
     }
-    @search_location = location
+  end
+
+  def data
+    Forecast.new(0, antipode_lat_long).get_antipode_forecast
+  end
+
+  def antipode_lat_long
+    Antipode.new(search_location).get_antipode
   end
 end
