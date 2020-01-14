c67443299d5aa04634b076c473d8a4e839c2aec5
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-14 15:21:11 -0700

    Update serializer and spec test to work with new forecast facade

--- spec/requests/api/v1/features/forecast_spec.rb
+++ spec/requests/api/v1/features/forecast_spec.rb
@@ -9,62 +9,9 @@ describe 'Get forecast data' do
         expect(response).to be_successful
 
         expect(:id).to_not be_empty
-        expect(:curent_time).to_not be_empty
-        expect(:current_summary).to_not be_empty
-        expect(:current_icon).to_not be_empty
-        expect(:current_temperature).to_not be_empty
-        expect(:current_apparent_temperature).to_not be_empty
-        expect(:current_humidity).to_not be_empty
-        expect(:current_uv_index).to_not be_empty
-        expect(:current_visability).to_not be_empty
-        expect(:current_percipitation_probability).to_not be_empty
-        expect(:current_percipitation_type).to_not be_empty
-        expect(:hourly_time).to_not be_empty
-        expect(:hourly_temperature).to_not be_empty
-        expect(:hourly_time_2).to_not be_empty
-        expect(:hourly_temperature_2).to_not be_empty
-        expect(:hourly_time_3).to_not be_empty
-        expect(:hourly_temperature_3).to_not be_empty
-        expect(:hourly_time_4).to_not be_empty
-        expect(:hourly_temperature_4).to_not be_empty
-        expect(:hourly_time_5).to_not be_empty
-        expect(:hourly_temperature_5).to_not be_empty
-        expect(:hourly_time_6).to_not be_empty
-        expect(:hourly_temperature_6).to_not be_empty
-        expect(:hourly_time_7).to_not be_empty
-        expect(:hourly_temperature_7).to_not be_empty
-        expect(:hourly_time_8).to_not be_empty
-        expect(:hourly_temperature_8).to_not be_empty
-        expect(:daily_date).to_not be_empty
-        expect(:daily_icon).to_not be_empty
-        expect(:daily_percipitation_type).to_not be_empty
-        expect(:daily_percipitation_probability).to_not be_empty
-        expect(:daily_temperature_low).to_not be_empty
-        expect(:daily_temperature_high).to_not be_empty
-        expect(:daily_date_2).to_not be_empty
-        expect(:daily_icon_2).to_not be_empty
-        expect(:daily_percipitation_type_2).to_not be_empty
-        expect(:daily_percipitation_probability_2).to_not be_empty
-        expect(:daily_temperature_low_2).to_not be_empty
-        expect(:daily_temperature_high_2).to_not be_empty
-        expect(:daily_date_3).to_not be_empty
-        expect(:daily_icon_3).to_not be_empty
-        expect(:daily_percipitation_type_3).to_not be_empty
-        expect(:daily_percipitation_probability_3).to_not be_empty
-        expect(:daily_temperature_low_3).to_not be_empty
-        expect(:daily_temperature_high_3).to_not be_empty
-        expect(:daily_date_4).to_not be_empty
-        expect(:daily_icon_4).to_not be_empty
-        expect(:daily_percipitation_type_4).to_not be_empty
-        expect(:daily_percipitation_probability_4).to_not be_empty
-        expect(:daily_temperature_low_4).to_not be_empty
-        expect(:daily_temperature_high_4).to_not be_empty
-        expect(:daily_date_5).to_not be_empty
-        expect(:daily_icon_5).to_not be_empty
-        expect(:daily_percipitation_type_5).to_not be_empty
-        expect(:daily_percipitation_probability_5).to_not be_empty
-        expect(:daily_temperature_low_5).to_not be_empty
-        expect(:daily_temperature_high_5).to_not be_empty
+        expect(:current_weather).to_not be_empty
+        expect(:hourly_weather).to_not be_empty
+        expect(:daily_weather).to_not be_empty
       end
 
       it 'Returns the url to an appropriate background image for a location' do
