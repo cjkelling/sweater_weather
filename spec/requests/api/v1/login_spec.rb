80994d235c306e145b4295d274b54c438395d46a
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-13 17:18:51 -0700

    Add a registered user before test the log in functionality.

--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'User Login' do
   describe 'User posts email and password to sessions endpoint' do
     it 'A successful request returns the users api key' do
+      post '/api/v1/users?email=whatever@example.com;password=password;password_confirmation=password'
+
       post '/api/v1/sessions?email=whatever@example.com;password=password'
 
       expect(response).to be_successful
