a2000834b8f7f1df0f25ce951e7b416e534761b3
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-14 15:51:14 -0700

    Create facade and serializer for background image

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    lat_long = Geocoding.new(params[:location]).latitude_longitude
-    render json: Background.new(lat_long).get_background
+    render json: BackgroundSerializer.new(BackgroundFacade.new(params[:location]))
   end
 end
