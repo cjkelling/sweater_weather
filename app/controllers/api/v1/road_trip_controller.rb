a6574adfcf2bc3ac467ed25f49248dfe67e8b4ef
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-14 08:52:30 -0700

    Add arrival forecast to road trip controller create action

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     if params[:api_key] && User.includes(:api_key).where('api_key = ?', params[:api_key])
       travel_time = RoadTrip.new(params[:origin], params[:destination]).get_time
-      render json: RoadTripSerializer.new(RoadTripFacade.new(params[:origin], params[:destination], travel_time))
+      arrival_forecast = Forecast.new(params[:destination]).get_weather_forecast
+      render json: RoadTripSerializer.new(RoadTripFacade.new(params[:origin], params[:destination], travel_time, arrival_forecast))
     else
       render json: { msg: 'Unauthorized' }, status: 401
     end
