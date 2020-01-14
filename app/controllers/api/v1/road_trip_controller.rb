b4b833d1dd90f3348f0ab80d33d88d76d5be992f
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-14 08:26:45 -0700

    Update road trip controller to return 401 if no api key is passed through the params

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    if User.includes(:api_key).where('api_key = ?', params[:api_key])
-      render json: RoadTrip.new(params[:origin, :destination])
+    if params[:api_key] && User.includes(:api_key).where('api_key = ?', params[:api_key])
+      travel_time = RoadTrip.new(params[:origin], params[:destination]).get_time
+      render json: RoadTripSerializer.new(RoadTripFacade.new(params[:origin], params[:destination], travel_time))
+    else
+      render json: { msg: 'Unauthorized' }, status: 401
     end
   end
 end
